458cc61b9bae29297b7d32418f93174533426960
Vatan Prakash <vatanprakash867@gmail.com>
2024-06-16 12:00:40 +0530

    changes

--- app/models.py
+++ app/models.py
@@ -2,15 +2,12 @@ from pymongo import MongoClient
 from app.config import Config
 from app.event_model import Event
 
-client = MongoClient(Config.MONGO_URI)
-db = client[Config.MONGO_DBNAME]
-collection = db['events']
 
-def save_event(event):
-    collection.insert_one(event.to_dict())
+def save_event(db, event):
+    db.events.insert_one(event.to_dict())
 
-def get_latest_events(limit=10):
-    events = list(collection.find().sort('timestamp', -1).limit(limit))
+def get_latest_events(db, limit=10):
+    events = list(db.events.find().sort('timestamp', -1).limit(limit))
     for event in events:
         event['_id'] = str(event['_id'])
     return events
